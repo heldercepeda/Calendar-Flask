498eeb7647ee166311a202276287c46b2688ccee
Helder <helder_cepeda@hotmail.com>
2020-07-22 17:47:12 +0100

    add variables to be sent to template

--- Calendar/views.py
+++ Calendar/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import render_template
 from Calendar import app
+from Calendar.functions.generic import list_for_calendar
 
 @app.route('/')
 @app.route('/home')
@@ -19,9 +20,12 @@ def register():
 
 @app.route('/calendar')
 def calendar():
+    final_list, next_month, next_month_year, prev_month, prev_month_year = list_for_calendar(month="December", year="2020")
     return render_template(
         'calendar.html',
-        day = datetime.today().strftime("%d"),
-        month = datetime.today().strftime("%B"),
-        year = datetime.today().strftime("%Y")
+        final_list=final_list,
+        next_month=next_month,
+        next_month_year=next_month_year,
+        prev_month=prev_month,
+        prev_month_year=prev_month_year
     )
