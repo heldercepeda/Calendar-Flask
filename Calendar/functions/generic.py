21485d2f79fbf9496e669f8022ac208cfc59f798
Helder <helder_cepeda@hotmail.com>
2020-07-22 17:46:42 +0100

    Rename function

--- Calendar/functions/generic.py
+++ Calendar/functions/generic.py
@@ -1,11 +1,29 @@
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+import numpy as np
 
 
-def get_calendar():
-    day = datetime.today().strftime("%d")
-    month = datetime.today().strftime("%B")
-    year = datetime.today().strftime("%Y")
-    print(day)
-    print(month)
-    print(year)
-    return None
+def list_for_calendar(month=None, year=None):
+    today = datetime.strftime(datetime.now(), '%Y-%m-%d')
+    if not month:
+        month = pd.to_datetime(today).strftime("%B")
+    if not year:
+        year = pd.to_datetime(today).strftime("%Y")
+    first_of_month = f'{year}-{month}-01'
+    print(first_of_month)
+    table_dates = pd.DataFrame(index=[i for i in range(6)], columns=[i for i in range(7)])
+    first = pd.to_datetime(first_of_month) - relativedelta(days=pd.to_datetime(first_of_month).dayofweek)
+    for i in range(table_dates.shape[0]):
+        for j in range(table_dates.shape[1]):
+            table_dates.iloc[i, j] = first + relativedelta(days=i * 7 + j)
+    final_list = []
+    for i in range(table_dates.shape[0]):
+        week_list = []
+        for j in range(table_dates.shape[1]):
+            week_list.append(table_dates.iloc[i, j].strftime("%d"))
+        final_list.append(week_list)
+    next_nonth = pd.to_datetime(first_of_month) + relativedelta(months=1)
+    prev_nonth = pd.to_datetime(first_of_month) - relativedelta(months=1)
+    return final_list, next_nonth.strftime("%B"), next_nonth.strftime("%Y"),\
+           prev_nonth.strftime("%B"), prev_nonth.strftime("%Y")
