a11d9fee146a5fc3839c7ba42c5a43e206cc1ba0
Helder <helder_cepeda@hotmail.com>
2020-07-22 17:07:16 +0100

    function get_calendar added

--- Calendar/functions/generic.py
+++ Calendar/functions/generic.py
@@ -1,1 +1,11 @@
+from datetime import datetime
 
+
+def get_calendar():
+    day = datetime.today().strftime("%d")
+    month = datetime.today().strftime("%B")
+    year = datetime.today().strftime("%Y")
+    print(day)
+    print(month)
+    print(year)
+    return None
