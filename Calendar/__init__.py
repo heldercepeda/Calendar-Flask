dd7e3e3af9dcbce4770fa1eb9466c056f2366afa
Helder <helder_cepeda@hotmail.com>
2020-07-23 12:08:37 +0100

    conf keys added

--- Calendar/__init__.py
+++ Calendar/__init__.py
@@ -3,6 +3,13 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "hgys8645fsrw2710skjg8762nmal0176aloq3710gtfn2739"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
 
 import Calendar.views
