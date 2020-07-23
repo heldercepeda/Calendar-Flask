2940be375dcfba65b02f48eea219a2dbb861e6a2
Helder <helder_cepeda@hotmail.com>
2020-07-23 12:40:48 +0100

    database created and models updated

--- Calendar/__init__.py
+++ Calendar/__init__.py
@@ -4,12 +4,13 @@ The flask application package.
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hgys8645fsrw2710skjg8762nmal0176aloq3710gtfn2739"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-
+login_manager = LoginManager(app)
 db = SQLAlchemy(app)
 
 import Calendar.views
